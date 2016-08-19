531a966d86616f71c48d1e83582b3d884bebd91a
foobar1643 <foobar76239@gmail.com>
2016-08-20 00:58:24 +0300

    Move PHP and PHP-FPM configs, add testhub config for nginx

--- usr/local/src/php7/configure-php.sh
+++ usr/local/src/php7/configure-php.sh
@@ -1,4 +1,4 @@
-### PRE INSTALL REQUIRED PACKAGES BEFORE CONFIGURING PHP ###
+### INSTALL REQUIRED PACKAGES BEFORE CONFIGURING PHP ###
 ## Everything that goes with --enable - does not require any external libs, except --enable-soap and --enable-zip
 
 # libxml-dev --enable-soap
@@ -22,8 +22,13 @@
 # mcrypt-dev - mcrypt lib
 # ssl-dev - SSL lib
 # curl4-openssl-dev - CURL \w SSL support
-sudo apt-get install libfcgi-dev libfcgi0ldbl libbz2-dev libxml2-dev libmcrypt-dev libssl-dev libcurl4-openssl-dev zlib1g-dev libmhash-dev libc-client2007e libc-client2007e-dev libpng-dev libjpeg-dev libwebp-dev libfreetype6-dev libpq-dev libxslt1-dev libgmp-dev libkrb5-dev libldap2-dev openssl
- 
+
+### DEV PACKAGES INSTALLATION COMMAND FOR apt ###
+# sudo apt-get install libfcgi-dev libfcgi0ldbl libbz2-dev libxml2-dev libmcrypt-dev libssl-dev libcurl4-openssl-dev zlib1g-dev libmhash-dev libc-client2007e-dev libpng-dev libjpeg-dev libwebp-dev libfreetype6-dev libpq-dev libxslt1-dev libgmp-dev libkrb5-dev libldap2-dev openssl
+
+### DEV PACKAGES INSTALLATION COMMAND FOR yum ###
+# yum install autoconf automake libtool bison fcgi-devel bzip2-devel libxml2-devel libmcrypt-devel libssl-devel libcurl-openssl-devel zlib-devel libmhash-devel libc-client-devel libpng-devel libjpeg-devel libwebp-devel freetype6-devel libxslt1-devel libgmp-devel libkrb5-devel libldap2-devel openssl libicu-devel
+
 ## NOTE ##
 # This will configure PHP to build with the following extensions:
   # libxml
@@ -41,15 +46,15 @@ sudo apt-get install libfcgi-dev libfcgi0ldbl libbz2-dev libxml2-dev libmcrypt-d
   # pcre-regex
   # zlib
   # bz2
-  # GD. Including 
+  # GD. Including
     # native-ttf
     # webp
     # jpeg
-    # gif 
+    # gif
     # png
     # freetype
   # gettext
-  # gmp  
+  # gmp
   # mhash
   # imap
   # imap-ssl
@@ -60,4 +65,4 @@ sudo apt-get install libfcgi-dev libfcgi0ldbl libbz2-dev libxml2-dev libmcrypt-d
   # pdo-mysql
   # openssl
   # curl
-./configure --enable-fpm --enable-libxml --enable-bcmath --enable-calendar --enable-exif --enable-ftp --enable-sockets --enable-zip --enable-soap --enable-mbstring --enable-intl --with-pcre-regex --with-xsl --with-zlib --with-zlib-dir=/usr --with-bz2 --with-gd --enable-gd-native-ttf --with-webp-dir=/usr --with-jpeg-dir=/usr --with-png-dir=/usr --with-freetype-dir=/usr --with-gettext --with-gmp --with-mhash --with-imap --with-mcrypt --with-pgsql --with-pdo-pgsql --with-mysqli --with-pdo-mysql --with-pear --with-kerberos --with-imap-ssl --with-openssl --with-config-file-path=/etc/php/ --with-config-file-scan-dir=/etc/php/conf.d/ --with-curl --with-fpm-user=www-data --with-fpm-group=www-data --with-ldap --with-xmlrpc
+./configure --enable-fpm --enable-libxml --enable-bcmath --enable-calendar --enable-exif --enable-ftp --enable-sockets --enable-zip --enable-soap --enable-mbstring --enable-intl --with-pcre-regex --with-xsl --with-zlib --with-zlib-dir=/usr --with-bz2 --with-gd --enable-gd-native-ttf --enable-gd-jis-conv --with-webp-dir=/usr --with-jpeg-dir=/usr --with-png-dir=/usr --with-freetype-dir=/usr --with-gettext --with-gmp --with-mhash --with-imap --with-mcrypt --with-pgsql=/var/lib/pgsql --with-pdo-pgsql --with-mysqli --with-pdo-mysql --with-pear --with-kerberos --with-imap-ssl --with-openssl --with-config-file-path=/etc/php/ --with-config-file-scan-dir=/etc/php/conf.d/ --with-curl --with-fpm-user=www-data --with-fpm-group=www-data --with-ldap --with-xmlrpc --with-libdir=lib64
